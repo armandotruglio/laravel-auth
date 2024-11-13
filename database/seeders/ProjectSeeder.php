7b49022f83784d24fa2cf251f306e38c334ddced
armandotruglio <armando.truglio1@gmail.com>
2024-11-13 16:55:51 +0100

    Created Model for Projects with migration and seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,16 +2,25 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Project;
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for($i=0; $i<20; $i++){
+            $project = new Project();
+            $project->title = $faker->sentence();
+            $project->description = $faker->paragraph();
+            $project->date = $faker->date();
+            $project->save();
+        }
     }
 }
